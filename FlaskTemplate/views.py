e0578b389c20fb6b01d2dfa4f833340052bd9005
Saloni-NITRR <76469961+Saloni-NITRR@users.noreply.github.com>
2021-01-11 23:43:03 +0530

    feat: Updated Index Page

--- FlaskTemplate/views.py
+++ FlaskTemplate/views.py
@@ -1,8 +1,3 @@
-"""
-Routes and views for the flask application.
-"""
-
-from datetime import datetime
 from flask import render_template
 from FlaskTemplate import app
 
@@ -12,26 +7,6 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+        name = "Ansh Srivastava"
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
