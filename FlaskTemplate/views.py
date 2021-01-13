14844be7e355e29d17bf7e807ebbc91031eb4ab9
Ansh Srivastava <ansh_srtv@outlook.com>
2021-01-13 23:38:00 +0530

    Update views.py

--- FlaskTemplate/views.py
+++ FlaskTemplate/views.py
@@ -1,12 +1,13 @@
 from flask import render_template
 from FlaskTemplate import app
 
+# TODO: Replace YOUR_NAME with your name e.g. Grace 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        name = "Ansh Srivastava"
+        name = "<YOUR_NAME>"
     )
 
